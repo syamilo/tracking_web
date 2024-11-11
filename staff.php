34d3ed8a826eca4e0f2c12d44e54ad37a3459b28
syamilo <sy4m1l15blaze@gmail.com>
2024-11-11 08:35:17 +0800

    uu

--- staff.php
+++ staff.php
@@ -12,7 +12,7 @@ if ($_SESSION['role'] != 'staff') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Staff Dashboard</title>
-    <link rel="stylesheet" href="staff.css">
+    <link rel="stylesheet" href="setaff.css">
 </head>
 <body>
     <!-- Header with user info and logout button -->
