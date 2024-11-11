3b0cfac283654c40bb4779405cd86aacb2c1f990
syamilo <sy4m1l15blaze@gmail.com>
2024-11-11 08:15:03 +0800

    test 3 kali 2

--- admin.php
+++ admin.php
@@ -24,33 +24,34 @@ if ($_SESSION['role'] != 'admin') {
 
     <!-- Main container for the dashboard with icons -->
     <div class="dashboard-container">
-        <h1>Staff Menu</h1>
-        <div class="dashboard-links">
-            <a href="activity_record.php" class="dashboard-item">
-                <img src="register_icon.png" alt="Activity Record Icon">
-                <p>Activity Record</p>
-            </a>
-            <a href="customer_record.php" class="dashboard-item">
-                <img src="icon_attendances.png" alt="Customer Record Icon">
-                <p>Customer Record</p>
-            </a>
-            <a href="staff_management.php" class="dashboard-item">
-                <img src="icon_monitor.png" alt="Staff Management Icon">
-                <p>Staff Management</p>
-            </a>
-            <a href="guider.php" class="dashboard-item">
-                <img src="report_icon.png" alt="Booking Date and Guider Icon">
-                <p>Booking Date and Guider</p>
-            </a>
-            <a href="report_record.php" class="dashboard-item">
-                <img src="icon_monitor.png" alt="report Record Icon">
-                <p>Report Recordt</p>
-            </a>
-            <a href="print_record.php" class="dashboard-item">
-                <img src="icon_monitor.png" alt="Print Record Icon">
-                <p>Print Record</p>
-            </a>
-        </div>
+    <h1>Staff Menu</h1>
+    <div class="dashboard-links">
+        <a href="activity_record.php" class="dashboard-item">
+            <img src="register_icon.png" alt="Activity Record Icon">
+            <p>Activity Record</p>
+        </a>
+        <a href="customer_record.php" class="dashboard-item">
+            <img src="icon_attendances.png" alt="Customer Record Icon">
+            <p>Customer Record</p>
+        </a>
+        <a href="staff_management.php" class="dashboard-item">
+            <img src="icon_monitor.png" alt="Staff Management Icon">
+            <p>Staff Management</p>
+        </a>
+        <a href="guider.php" class="dashboard-item">
+            <img src="report_icon.png" alt="Booking Date and Guider Icon">
+            <p>Booking Date and Guider</p>
+        </a>
+        <a href="report_record.php" class="dashboard-item">
+            <img src="icon_monitor.png" alt="Report Record Icon">
+            <p>Report Record</p>
+        </a>
+        <a href="print_record.php" class="dashboard-item">
+            <img src="icon_monitor.png" alt="Print Record Icon">
+            <p>Print Record</p>
+        </a>
     </div>
+</div>
+
 </body>
 </html>
